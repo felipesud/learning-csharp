8691bf354890d10a32f49ed542359c54ebc3c0e6
ebelfel <felipe.belisario@ericsson.com>
2023-10-19 11:06:31 -0300

    new activities made

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,21 +1,9 @@
-﻿
-/*
-string[] fraudulentOrderIDs = new string[3];
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-// fraudulentOrderIDs[3] = "D000";
-*/
+﻿string[] id = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
+foreach(string i in id )
+{
+    if(i.StartsWith("B")){
+        Console.WriteLine(i);
+    }
+}
 
-
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
-
-fraudulentOrderIDs[0] = "F000";
-
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-
-Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process. ");
